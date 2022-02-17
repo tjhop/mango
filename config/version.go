47c85069b0e5b085c8dee297eb22c681dd88ceed
TJ Hoplock <t.hoplock@gmail.com>
2022-02-17 12:36:31 -0500

    ci: init goreleaser + github actions, add systemd unit and config

---
+++ config/version.go
@@ -0,0 +1,7 @@
+package config
+
+var (
+	Version   string // will be populated by linker during `go build`
+	BuildDate string // will be populated by linker during `go build`
+	Commit    string // will be populated by linker during `go build`
+)
