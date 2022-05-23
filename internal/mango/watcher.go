4465e3e561e18b5119aa9a70686fb84db192be9c
TJ Hoplock <t.hoplock@gmail.com>
2022-05-23 12:54:06 -0400

    feat: add prom metrics for mango watcher fs events/errors

--- internal/mango/watcher.go
+++ internal/mango/watcher.go
@@ -7,8 +7,27 @@ import (
 	_ "github.com/tjhop/mango/internal/logging"
 
 	"github.com/fsnotify/fsnotify"
+	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/client_golang/prometheus/promauto"
 	log "github.com/sirupsen/logrus"
 	"github.com/spf13/viper"
+)
+
+var (
+	// prometheus metrics
+	metricMangoWatcherEventsTotal = promauto.NewCounter(
+		prometheus.CounterOpts{
+			Name: "mango_mango_watcher_events_total",
+			Help: "Total number of filesystem events detected within mango tree",
+		},
+	)
+
+	metricMangoWatcherErrorsTotal = promauto.NewCounter(
+		prometheus.CounterOpts{
+			Name: "mango_mango_watcher_errors_total",
+			Help: "Total number of errors received watching for filesystem events within mango tree",
+		},
+	)
 )
 
 func NewMangoWatcher() {
@@ -36,6 +55,8 @@ func NewMangoWatcher() {
 							"event": event,
 						}).Debug("Filesystem event received in mango tree directory, reloading mango tree")
 
+						metricMangoWatcherEventsTotal.Inc()
+
 						ReloadTree(mTree)
 					}
 
@@ -43,6 +64,8 @@ func NewMangoWatcher() {
 					log.WithFields(log.Fields{
 						"error": err,
 					}).Error("Failed to handle event from fsnotify watcher")
+
+					metricMangoWatcherErrorsTotal.Inc()
 				}
 			}
 		}()
@@ -51,7 +74,7 @@ func NewMangoWatcher() {
 		if err != nil {
 			log.WithFields(log.Fields{
 				"error": err,
-				"path": mTree,
+				"path":  mTree,
 			}).Error("Failed to add mango tree directory to mango watcher")
 		}
 		wg.Wait()
