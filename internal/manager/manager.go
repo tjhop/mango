32eee0430271a9d3461f6a948bd8624d076e6b46
TJ Hoplock <t.hoplock@gmail.com>
2022-11-16 00:07:06 -0500

    chore: improve manager logging
    - Manager struct now embeds a logger, constructor updated to set it
    - updated all calls to global log package to use the manager's logger
    - add early return from run managers/directives funcs to bail if there
      is no work and _explicitly log that to the user_. the range loop
    handles a 0 length slice fine, but this improves the "I successfully
    have nothing to do" style logging.

--- internal/manager/manager.go
+++ internal/manager/manager.go
@@ -11,6 +11,7 @@ import (
 // Manager contains fields related to track and execute runnable modules and statistics.
 type Manager struct {
 	id         string
+	logger     *log.Entry
 	modules    []inventory.Module
 	directives []inventory.DirectiveScript
 }
@@ -18,26 +19,49 @@ type Manager struct {
 func (m *Manager) String() string { return m.id }
 
 // NewManager returns a new Manager struct instantiated with the given ID
-func NewManager(id string) Manager {
-	return Manager{id: id}
+func NewManager(id string) *Manager {
+	return &Manager{
+		id: id,
+		logger: log.WithFields(log.Fields{
+			"manager": id,
+		}),
+	}
 }
 
 // Reload accepts a struct that fulfills the inventory.Store interface and
 // reloads the hosts modules/directives from the inventory
 func (m *Manager) Reload(inv inventory.Store) {
-	m.modules = inv.GetModulesForSelf()
-	m.directives = inv.GetDirectivesForSelf()
+	m.logger.Info("Reloading items from inventory")
+
+	modules := inv.GetModulesForSelf()
+	m.logger.WithFields(log.Fields{
+		"old":       m.modules,
+		"new":       modules,
+	}).Debug("Reloading modules from inventory")
+	m.modules = modules
+
+	directives := inv.GetDirectivesForSelf()
+	m.logger.WithFields(log.Fields{
+		"old":       m.directives,
+		"new":       directives,
+	}).Debug("Reloading directives from inventory")
+	m.directives = directives
 }
 
 // RunDirectives runs all of the directive scripts being managed by the Manager
 func (m *Manager) RunDirectives(ctx context.Context) {
+	if len(m.directives) <= 0 {
+		m.logger.Info("No Directives to run")
+		return
+	}
+
 	for _, d := range m.directives {
-		log.WithFields(log.Fields{
+		m.logger.WithFields(log.Fields{
 			"path": d,
 		}).Info("Running directive")
 
 		if err := d.Run(ctx); err != nil {
-			log.WithFields(log.Fields{
+			m.logger.WithFields(log.Fields{
 				"path": d,
 			}).Error("Directive failed")
 		}
@@ -46,14 +70,19 @@ func (m *Manager) RunDirectives(ctx context.Context) {
 
 // RunModules runs all of the modules being managed by the Manager
 func (m *Manager) RunModules(ctx context.Context) {
-	for _, d := range m.modules {
-		log.WithFields(log.Fields{
-			"path": d,
-		}).Info("Running module")
+	if len(m.modules) <= 0 {
+		m.logger.Info("No Modules to run")
+		return
+	}
 
-		if err := d.Run(ctx); err != nil {
-			log.WithFields(log.Fields{
-				"path": d,
+	for _, mod := range m.modules {
+		m.logger.WithFields(log.Fields{
+			"path": mod,
+		}).Info("Running Module")
+
+		if err := mod.Run(ctx); err != nil {
+			m.logger.WithFields(log.Fields{
+				"path": mod,
 			}).Error("Module failed")
 		}
 	}
