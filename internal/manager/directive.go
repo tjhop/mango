4631502b765ddfdff74f664c221483d21d8ee923
TJ Hoplock <t.hoplock@gmail.com>
2023-08-23 22:40:34 -0400

    fix: only run directive if modified within <24h ago
    This is always how directives were supposed to work. I know I used to
    check timestamps differently/elsewhere, I seemingly forgot to re-add the
    freshness check into the current manager implementation.

--- internal/manager/directive.go
+++ internal/manager/directive.go
@@ -3,6 +3,7 @@ package manager
 import (
 	"context"
 	"fmt"
+	"os"
 	"time"
 
 	"github.com/prometheus/client_golang/prometheus"
@@ -34,32 +35,40 @@ func (mgr *Manager) ReloadDirectives(ctx context.Context) {
 // RunDirective is responsible for actually executing a directive, using the `shell`
 // package.
 func (mgr *Manager) RunDirective(ctx context.Context, ds Directive) error {
-	ctx, runID := getOrSetRunID(ctx)
-	applyStart := time.Now()
-	labels := prometheus.Labels{
-		"directive": ds.String(),
-	}
-	metricManagerDirectiveRunTimestamp.With(labels).Set(float64(applyStart.Unix()))
-
-	hostVarsMap := shell.MakeVariableMap(mgr.hostVariables)
-	allTemplateData := mgr.getTemplateData(ctx, ds.String(), hostVarsMap, nil, hostVarsMap)
-
-	renderedScript, err := templateScript(ctx, ds.String(), allTemplateData, mgr.funcMap)
+	file, err := os.Stat(ds.String())
 	if err != nil {
-		return fmt.Errorf("Failed to template script: %s", err)
+		return fmt.Errorf("Failed to stat directive script %s: %s", ds.String(), err)
 	}
 
-	err = shell.Run(ctx, runID, ds.String(), renderedScript, nil)
+	// only run directive if modified within last 24h
+	if file.ModTime().After(time.Now().Add(-(time.Hour * 24))) {
+		ctx, runID := getOrSetRunID(ctx)
+		applyStart := time.Now()
+		labels := prometheus.Labels{
+			"directive": ds.String(),
+		}
+		metricManagerDirectiveRunTimestamp.With(labels).Set(float64(applyStart.Unix()))
 
-	// update metrics regardless of error, so do them before handling error
-	applyEnd := time.Since(applyStart)
-	metricManagerDirectiveRunSuccessTimestamp.With(labels).Set(float64(applyStart.Unix()))
-	metricManagerDirectiveRunDuration.With(labels).Set(float64(applyEnd))
-	metricManagerDirectiveRunTotal.With(labels).Inc()
+		hostVarsMap := shell.MakeVariableMap(mgr.hostVariables)
+		allTemplateData := mgr.getTemplateData(ctx, ds.String(), hostVarsMap, nil, hostVarsMap)
 
-	if err != nil {
-		metricManagerDirectiveRunFailedTotal.With(labels).Inc()
-		return fmt.Errorf("Failed to apply directive: %v", err)
+		renderedScript, err := templateScript(ctx, ds.String(), allTemplateData, mgr.funcMap)
+		if err != nil {
+			return fmt.Errorf("Failed to template script: %s", err)
+		}
+
+		err = shell.Run(ctx, runID, ds.String(), renderedScript, nil)
+
+		// update metrics regardless of error, so do them before handling error
+		applyEnd := time.Since(applyStart)
+		metricManagerDirectiveRunSuccessTimestamp.With(labels).Set(float64(applyStart.Unix()))
+		metricManagerDirectiveRunDuration.With(labels).Set(float64(applyEnd))
+		metricManagerDirectiveRunTotal.With(labels).Inc()
+
+		if err != nil {
+			metricManagerDirectiveRunFailedTotal.With(labels).Inc()
+			return fmt.Errorf("Failed to apply directive: %v", err)
+		}
 	}
 
 	return nil
