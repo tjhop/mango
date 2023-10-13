0db542215744b3e28d9c4e7a5f3666d90fce43c9
TJ Hoplock <t.hoplock@gmail.com>
2023-10-13 01:03:29 -0400

    ref: simply GetHostname wrapper to disregard error

--- pkg/utils/utils.go
+++ pkg/utils/utils.go
@@ -72,17 +72,8 @@ func ReadFileLines(path string) chan FileLine {
 	return lines
 }
 
-// GetHostname is a wrapper around `os.Hostname()`. Since the hostname is how
-// Mango determines what configurations are applicable to the running system,
-// the hostname is critical. It returns the hostname as a string if successful,
-// and exits fatally if it fails.
+// GetHostname is a wrapper around `os.Hostname()` that disregards the error
 func GetHostname() string {
-	h, err := os.Hostname()
-	if err != nil {
-		log.WithFields(log.Fields{
-			"error": err,
-		}).Fatal("Failed to get hostname! Mango cannot determine the system's identity and is unable to determine what configurations are applicable.")
-	}
-
+	h, _ := os.Hostname()
 	return h
 }
